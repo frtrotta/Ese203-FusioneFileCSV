40eef8af082eef298c79832a3efcc5e7bb7056aa
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-04-15 18:23:03 +0200

    Completata

--- integra.sh
+++ integra.sh
@@ -3,19 +3,24 @@
 # In questo script l'elenco delle cartelle da trattare è definito con seq
 # Il comando read utilizza l'opzione -r
 
-for filiale in `seq 5` 
+prima_filiale=1
+
+for filiale in `ls -d */`
+#for filiale in */
+# La sintassi precedente e' anche valida.
 do
-	filiale=0$filiale
+	filiale=${filiale:0:${#filiale}-1}
 	prima_riga=1
 	while read -r linea
 	do
 		if [ $prima_riga -eq 1 ]
 		then
 			prima_riga=0
-			if [ "$filiale" == '01' ]
+			if [ $prima_filiale -eq 1 ]
 			then
 				# Aggiungo l'intestazione per il nuovo campo come ultima colonna
 				echo "${linea};filiale" > merged.csv
+				prima_filiale=0
 			fi
 		else
 			# Riporto la linea, aggiungendo il codice della filiale
